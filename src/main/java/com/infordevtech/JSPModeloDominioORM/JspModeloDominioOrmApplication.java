3e20f79ec147706efd17735a86a55c6c82bcb96b
P3dr0S17v5 <pedro.graffic@gmail.com>
2025-03-20 17:39:18 +0000

    First Commit

---
+++ src/main/java/com/infordevtech/JSPModeloDominioORM/JspModeloDominioOrmApplication.java
@@ -0,0 +1,13 @@
+package com.infordevtech.JSPModeloDominioORM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspModeloDominioOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspModeloDominioOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infordevtech/JSPModeloDominioORM/JspModeloDominioOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infordevtech.JSPModeloDominioORM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspModeloDominioOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
